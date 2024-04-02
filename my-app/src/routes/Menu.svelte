c3b605853f1de32a41b4d33731fe23407d3056b1
DefinitelyAlec <alecparent@rocketmail.com>
2024-04-02 00:42:17 -0600

    made navbar a bit more dynamic, ready for animations

--- my-app/src/routes/Menu.svelte
+++ my-app/src/routes/Menu.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { fly, scale } from 'svelte/transition';
+    import { quadOut } from 'svelte/easing';
 
     /**
      * @type {any}
@@ -8,7 +10,18 @@
 
 
 {#if open}
-    <nav class="">
+    <div>
+        {#each ['Home', 'Projects', 'Contact'] as link, i}
+            <div class="btn" transition:fly={{ y: -15, delay: 50 * i}}>
+                {#if link=='Home'}
+                    <a href="/">{link}</a>
+                {:else}
+                    <a href="/{link.toLowerCase()}">{link}</a>
+                {/if}
+            </div>
+        {/each}
+    </div>
+    <!-- <nav class="">
         <div class="px-3 py-1">
             <div class="btn">
                 <a href="/">Home</a>
@@ -24,5 +37,5 @@
                 <a href="/contact">Contact</a>
             </div>
         </div>
-    </nav>
+    </nav> -->
 {/if}
