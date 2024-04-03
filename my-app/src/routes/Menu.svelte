cebd37fb0b01d3bfbe1be790646eee517b038038
DefinitelyAlec <alecparent@rocketmail.com>
2024-04-02 22:33:07 -0600

    removed non-functional transition

--- my-app/src/routes/Menu.svelte
+++ my-app/src/routes/Menu.svelte
@@ -14,7 +14,7 @@
     <div class="grid grid-cols-1 gap-4 place-items-stretch">
         {#each ['Home', 'Projects', 'Contact'] as link, i}
             <!-- Make each button have a minimum width so that they are all the same width -->
-            <div class="btn max-w-20" transition:fly={{ y: -15, delay: 50 * i}}>
+            <div class="btn max-w-20">
                 {#if link=='Home'}
                     <a href="/">{link}</a>
                 {:else}
