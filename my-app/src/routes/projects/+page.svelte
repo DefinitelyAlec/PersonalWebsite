2c81b21fbee2f815deecfda79d9f76f570f8ad0d
SenseiDeath <alecparent@rocketmail.com>
2024-04-16 20:54:54 -0700

    added placeholder elements for projects

--- my-app/src/routes/projects/+page.svelte
+++ my-app/src/routes/projects/+page.svelte
@@ -1,3 +1,13 @@
 <h1 class="text-3xl font-bold text-center py-12">
     Projects
 </h1>
+
+<h2 class="text-2xl font-bold py-12">
+    Project 1 placeholder
+</h2>
+<h2 class="text-2xl font-bold py-12">
+    Project 2 placeholder
+</h2>
+<h2 class="text-2xl font-bold py-12">
+    Project 3 placeholder
+</h2>
