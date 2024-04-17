6c1e64afe963061689c9eb13d570caa464e99760
SenseiDeath <alecparent@rocketmail.com>
2024-04-16 21:24:50 -0700

    started adding RPS sim as an iframe for the projects

--- my-app/src/routes/projects/+page.svelte
+++ my-app/src/routes/projects/+page.svelte
@@ -5,6 +5,8 @@
 <h2 class="text-2xl font-bold py-12">
     Project 1 placeholder
 </h2>
+<iframe src="https://rps-sim.netlify.app/" title="Rock Paper Scissors Simulator" width="800em" height="850em"></iframe>
+
 <h2 class="text-2xl font-bold py-12">
     Project 2 placeholder
 </h2>
