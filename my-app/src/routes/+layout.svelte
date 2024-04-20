e176565bef1573cb903808b58e5d1e1551909a0a
senseideath <alecparent@rocketmail.com>
2024-04-20 00:39:32 -0600

    changed location of the circle to be in the base layout document

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -3,18 +3,48 @@
 
   import Header from "./Header.svelte";
 
+
+  let xPos = 0;
+  let yPos = 0;
+
+  function mouseMoved(event: MouseEvent) {
+      xPos = event.clientX;
+      yPos = event.clientY;
+  }
 </script>
 
 <main>
-  <Header />
+  <div role="document" class="page-container" on:mousemove={mouseMoved}>
+    <div class="circle" style="top: {yPos-200}px; left: {xPos-200}px;"></div>
+    
+    <Header />
 
-  <slot />
+    <slot />
+  </div>
+
+
 </main>
 
-<style>
+<style lang="postcss">
   main {
     overflow: hidden;
 
   }
 
+  .page-container {
+      overflow: hidden;
+      position: relative; /* prevents scrollbars */
+  }
+
+  .circle {
+      position: absolute;
+      width: 400px; /* Increased size */
+      height: 400px; /* Increased size */
+      border-radius: 50%;
+      /* Background made of translucent pixels */
+      background-image: radial-gradient(circle, rgba(0,0,255,0.2) 0%, rgba(0,0,255,0) 60%);
+      background-size: 100% 100%;
+      background-repeat: no-repeat;
+      pointer-events: none; /* Allows mouse events to pass through */
+  }
 </style>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
     let xPos = 0;
     let yPos = 0;
 
@@ -6,20 +6,21 @@
         xPos = event.clientX;
         yPos = event.clientY;
     }
-</script>
+</script> -->
 
 
-<div role="document" class="page-container" on:mousemove={mouseMoved}>
+<!-- <div role="document" class="page-container" on:mousemove={mouseMoved}> -->
     <h1 class="text-3xl font-bold text-center py-12">
         Alec Parent's Personal Website
     </h1>
-    <div class="circle" style="top: {yPos-200}px; left: {xPos-200}px;"></div>
+    <!-- <div class="circle" style="top: {yPos-200}px; left: {xPos-200}px;"></div>
 
-</div>
+</div> -->
 
 
-<style lang="postcss">
+<!-- <style lang="postcss">
     .page-container {
+        overflow: hidden;
         position: relative; /* prevents scrollbars */
     }
 
@@ -34,4 +35,4 @@
         background-repeat: no-repeat;
         pointer-events: none; /* Allows mouse events to pass through */
     }
-</style>
+</style> -->
