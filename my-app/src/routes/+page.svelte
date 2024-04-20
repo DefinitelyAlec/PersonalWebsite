2d0614e002d9a28f768e5045dacaca3c7f926016
senseideath <alecparent@rocketmail.com>
2024-04-20 00:12:35 -0600

    flushed out circle to be more appealing

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,12 +1,37 @@
+<script>
+    let xPos = 0;
+    let yPos = 0;
 
-<div class="page-container">
+    function mouseMoved(event) {
+        xPos = event.clientX;
+        yPos = event.clientY;
+    }
+</script>
+
+
+<div role="document" class="page-container" on:mousemove={mouseMoved}>
     <h1 class="text-3xl font-bold text-center py-12">
         Alec Parent's Personal Website
     </h1>
+    <div class="circle" style="top: {yPos-200}px; left: {xPos-200}px;"></div>
+
 </div>
+
 
 <style lang="postcss">
     :global(body) {
         @apply bg-zinc-900;
+    }
+
+    .circle {
+        position: absolute;
+        width: 400px; /* Increased size */
+        height: 400px; /* Increased size */
+        border-radius: 50%;
+        /* Background made of translucent pixels */
+        background-image: radial-gradient(circle, rgba(0,0,255,0.2) 0%, rgba(0,0,255,0) 60%);
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        pointer-events: none; /* Allows mouse events to pass through */
     }
 </style>
