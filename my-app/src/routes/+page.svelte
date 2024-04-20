ded61f97cbf959a7afec20855f541058e8b695c1
senseideath <alecparent@rocketmail.com>
2024-04-19 23:49:32 -0600

    placeholder for the main content page's background.

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,3 +1,12 @@
-<h1 class="text-3xl font-bold text-center py-12">
-    Alec Parent's Personal Website
-</h1>
+
+<div class="page-container">
+    <h1 class="text-3xl font-bold text-center py-12">
+        Alec Parent's Personal Website
+    </h1>
+</div>
+
+<style lang="postcss">
+    :global(body) {
+        @apply bg-zinc-900;
+    }
+</style>
