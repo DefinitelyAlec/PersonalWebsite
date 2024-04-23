8f456823eacdafccaa3fa8a8690593d6f637982a
senseideath <alecparent@rocketmail.com>
2024-04-23 00:28:23 -0600

    added dropdowns for window size and got rid of center of navbar

--- my-app/src/routes/Header.svelte
+++ my-app/src/routes/Header.svelte
@@ -4,6 +4,9 @@
   import MdiMoonWaningCrescent from '~icons/mdi/moon-waning-crescent';
   import { theme } from '../stores';
   import { onMount } from 'svelte';
+  
+  let dropdownVisibleStart = false;
+  // let centerVisible = true;
 
   function toggleTheme(){
     theme.set($theme=="light"? "dark" : "light");
@@ -15,12 +18,25 @@
     theme.subscribe(value => {
       document.documentElement.setAttribute('data-theme', value);
     });
+    
+    // Check if buttons overflow and toggle dropdown visibility accordingly
+    const navbarStart = document.querySelector('.navbar-start');
+    const navbarEnd = document.querySelector('.navbar-end');
+    // const navbarCenter = document.querySelector('.navbar-center');
+
+    const checkOverflow = () => {
+      dropdownVisibleStart = navbarStart.scrollWidth > navbarStart.clientWidth;
+      centerVisible = navbarCenter.scrollWidth > navbarCenter.clientWidth;
+    };
+
+    window.addEventListener('resize', checkOverflow);
+    checkOverflow();
   });
 </script>
 
 <div class="navbar bg-base-100">
   <div class="navbar-start">
-    <!-- <div class="dropdown">
+    <div class="dropdown" class:hidden={!dropdownVisibleStart}>
       <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -39,30 +55,72 @@
       <ul
         class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
       >
-        <li><a href="/">Homepage</a></li>
-        <li><a href="/projects">Projects</a></li>
+        <li><a href="/" class="home-btn">Home</a></li>
+        <li><a href="/projects" class="projects-btn">Projects</a></li>
       </ul>
-    </div> -->
+    </div>
 
-    <a class="btn btn-ghost text-xl" href="/">Home</a>
-    <a class="btn btn-ghost text-xl" href="/projects">Projects</a>
+    <a class="btn btn-ghost text-xl home-btn" href="/" class:hidden={dropdownVisibleStart}>Home</a>
+    <a class="btn btn-ghost text-xl projects-btn" href="/projects" class:hidden={dropdownVisibleStart}>Projects</a>
     
   </div>
-  <div class="navbar-center">
+  <!-- <div class="navbar-center" class:hidden={!centerVisible}>
     <a class="btn btn-ghost text-xl" href="/">Alec Parent</a>
-  </div>
+  </div> -->
   <div class="navbar-end">
-    <a title="Email" href="mailto:alecparent@rocketmail.com">
+    <div class="dropdown" class:hidden={!dropdownVisibleStart}>
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h7"
+          /></svg
+        >
+      </div>
+      <ul
+        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        style="margin-left: auto; margin-right: auto;"
+      >
+        <li>
+          <a title="Email" href="mailto:alecparent@rocketmail.com">
+            <button class="btn btn-ghost btn-circle">
+              <MdiEmailOutline/>
+            </button>
+          </a>
+        </li>
+        <li>
+          <a title="GitHub" target="_blank" rel="noopener noreferrer" href="https://github.com/DefinitelyAlec">
+            <button class="btn btn-ghost btn-circle">          
+              <MdiGithub/>
+            </button>
+          </a>
+        </li>
+        <li>
+          <button title="Toggle Theme" class="btn btn-ghost btn-circle" on:click={toggleTheme}>
+            <MdiMoonWaningCrescent/>
+          </button>
+        </li>
+      </ul>
+    </div>
+
+    <a title="Email" href="mailto:alecparent@rocketmail.com" class:hidden={dropdownVisibleStart}>
       <button class="btn btn-ghost btn-circle">
           <MdiEmailOutline/>
       </button>
     </a>
-    <a title="GitHub" target="_blank" rel="noopener noreferrer" href="https://github.com/DefinitelyAlec">
+    <a title="GitHub" target="_blank" rel="noopener noreferrer" href="https://github.com/DefinitelyAlec" class:hidden={dropdownVisibleStart}>
       <button class="btn btn-ghost btn-circle">          
         <MdiGithub/>
       </button>
     </a>
-    <button title="Toggle Theme" class="btn btn-ghost btn-circle" on:click={toggleTheme}>
+    <button title="Toggle Theme" class="btn btn-ghost btn-circle" on:click={toggleTheme} class:hidden={dropdownVisibleStart}>
       <MdiMoonWaningCrescent/>
     </button>
   </div>
