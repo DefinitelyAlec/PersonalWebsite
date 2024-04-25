3e5879f2b26dc02faaca22f7376f6f78ac02dade
DefinitelyAlec <alecparent@rocketmail.com>
2024-04-24 23:11:21 -0600

    started drafting how I want to add descriptions to the projects.

--- my-app/src/routes/projects/+page.svelte
+++ my-app/src/routes/projects/+page.svelte
@@ -18,7 +18,19 @@
     href="https://movingforwardutah.com/">Info Website for a Therapy Center</a>
 </h2>
 
+<div class="py-12">
+    <h2 class="py-6">
+        <a class="text-2xl underline text-blue-600 hover:text-blue-800 visited:text-purple-600 font-bold" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        href="https://mrshobby.com/">Ecommerce Website for a Hobby Shop</a>
+    </h2>
+    <h3>My Involvement:</h3>
+    <ul>
+        <li>IN PROGRESS: Made a React API to get the BigCommerce site to properly pull product based off their category</li>
+    </ul>
+</div>
 
 <h2 class="text-2xl font-bold py-12">
-    Project 3 placeholder
+    Python batch installer
 </h2>
