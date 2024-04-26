c2db4a8cb330b5237916a3634e92b2236e0e04dd
DefinitelyAlec <alecparent@rocketmail.com>
2024-04-25 22:38:32 -0600

    added accordian style dropdowns to describe projects.

--- my-app/src/routes/projects/+page.svelte
+++ my-app/src/routes/projects/+page.svelte
@@ -2,51 +2,67 @@
     Projects
 </h1>
 
-<h2 class="py-12">
-    <a class="text-2xl underline text-blue-600 hover:text-blue-800 visited:text-purple-600 font-bold" 
-    target="_blank" 
-    rel="noopener noreferrer" 
-    href="https://rps-sim.netlify.app/">Rock Paper Scissors Battle Royale</a>
-</h2>
-
 <!-- <iframe src="https://rps-sim.netlify.app/" title="Rock Paper Scissors Simulator" width="800em" height="850em"></iframe> -->
 
-<h2 class="py-12">
-    <a class="text-2xl underline text-blue-600 hover:text-blue-800 visited:text-purple-600 font-bold" 
-    target="_blank" 
-    rel="noopener noreferrer" 
-    href="https://movingforwardutah.com/">Info Website for a Therapy Center</a>
-</h2>
-
-<div class="collapse collapse-arrow bg-base-200">
-    <input type="checkbox" checked/> 
-    <div class="collapse-title text-xl font-medium">
-        Ecommerce Website for a Hobby Shop
-    </div>
-    <div class="collapse-content prose"> 
-      <ul>
-        <li><a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600 font-bold" 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            href="https://mrshobby.com/">Website link</a></li>
-        <li>Made a React API to get the BigCommerce site to properly pull product based off their category</li>
-        <li>Helped translate non-technical desires for features into a technical roadmap</li>
-        <li>Established a consistent and reliable data pipeline between the POS and website</li>
-    </ul>
+<div class="py-6">
+    <div class="collapse collapse-arrow bg-base-200">
+        <input type="checkbox" checked/> 
+        <div class="collapse-title text-xl font-medium">
+            Rock Paper Scissors Battle Royale
+        </div>
+        <div class="collapse-content prose"> 
+        <ul>
+            <li><a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600 font-bold" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                href="https://rps-sim.netlify.app/">Website link</a></li>
+            <li>Built out game in pure HTML/JS/CSS</li>
+            <li>Leveraged ChatGPT to exponentially speed up development by generating boilerplate code and correcting/adding details</li>
+            <li>Built out vector based tracking system for all entities in a &lt;canvas&gt; component</li>
+        </ul>
+        </div>
     </div>
 </div>
 
-<div class="py-12">
-    <h2 class="py-6">
-        <a class="text-2xl underline text-blue-600 hover:text-blue-800 visited:text-purple-600 font-bold" 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        href="https://mrshobby.com/">Ecommerce Website for a Hobby Shop</a>
-    </h2>
-    <h3>My Involvement:</h3>
-    <ul>
-        <li>IN PROGRESS: Made a React API to get the BigCommerce site to properly pull product based off their category</li>
-    </ul>
+<div class="py-6">
+    <div class="collapse collapse-arrow bg-base-200">
+        <input type="checkbox" checked/> 
+        <div class="collapse-title text-xl font-medium">
+            Info Website for a Therapy Center
+        </div>
+        <div class="collapse-content prose"> 
+        <ul>
+            <li><a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600 font-bold" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                href="https://movingforwardutah.com/">Website link</a></li>
+            <li>Worked with Wordpress to improve existing site structure and content</li>
+            <li>Built out custom HTML/JS/CSS solutions to fix what Wordpress couldn't deliver on</li>
+            <li>Optimized the website caching, SEO, and general performance to load up to 10x faster</li>
+            <li>Operated on a strict deadline to deliver on client needs</li>
+        </ul>
+        </div>
+    </div>
+</div>
+
+<div class="py-6">
+    <div class="collapse collapse-arrow bg-base-200">
+        <input type="checkbox" checked/> 
+        <div class="collapse-title text-xl font-medium">
+            Ecommerce Website for a Hobby Shop
+        </div>
+        <div class="collapse-content prose"> 
+        <ul>
+            <li><a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600 font-bold" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                href="https://mrshobby.com/">Website link</a></li>
+            <li>Made a React API to get the BigCommerce site to properly pull product based off their category</li>
+            <li>Helped translate non-technical desires for features into a technical roadmap</li>
+            <li>Established a consistent and reliable data pipeline between the POS and website</li>
+        </ul>
+        </div>
+    </div>
 </div>
 
 <h2 class="text-2xl font-bold py-12">
