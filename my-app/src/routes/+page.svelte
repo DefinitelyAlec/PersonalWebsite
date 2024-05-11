4ec29244f317319bdaf412a695ed2a92025bcaeb
DefinitelyAlec <alecparent@rocketmail.com>
2024-05-11 01:31:32 -0600

    added large email and GitHub buttons to fill the Home page

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,3 +1,23 @@
+<script lang="ts">
+    import MdiEmailOutline from '~icons/mdi/email-outline';
+    import MdiGithub from '~icons/mdi/github';
+</script>
+
 <h1 class="text-3xl font-bold text-center py-12">
     Alec Parent's Personal Website
 </h1>
+
+<div id="EmailGitHubContainer" class="flex justify-around">
+    <div id="EmailContainer" class="btn btn-ghost large-icon h-28">
+        <MdiEmailOutline/>
+    </div>
+    <div id="GitHubContainer" class="btn btn-ghost large-icon h-28">
+        <MdiGithub/>
+    </div>
+</div>
+
+<style>
+    .large-icon {
+        font-size: 5em;
+    }
+</style>
