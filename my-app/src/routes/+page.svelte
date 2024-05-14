1209188e894d39da97409b2d2a97f0010534b588
DefinitelyAlec <alecparent@rocketmail.com>
2024-05-13 21:50:55 -0600

    added links to email and github icons on main page.

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -8,12 +8,16 @@
 </h1>
 
 <div id="EmailGitHubContainer" class="flex justify-around">
-    <div id="EmailContainer" class="btn btn-ghost large-icon h-28">
-        <MdiEmailOutline/>
-    </div>
-    <div id="GitHubContainer" class="btn btn-ghost large-icon h-28">
-        <MdiGithub/>
-    </div>
+    <a title="Email" href="mailto:alecparent@rocketmail.com">
+        <div id="EmailContainer" class="btn btn-ghost large-icon h-28">
+            <MdiEmailOutline/>
+        </div>
+    </a>
+    <a title="GitHub" target="_blank" rel="noopener noreferrer" href="https://github.com/DefinitelyAlec">
+        <div id="GitHubContainer" class="btn btn-ghost large-icon h-28">
+            <MdiGithub/>
+        </div>
+    </a>
 </div>
 
 <style>
